2154e7264888b4a7e1a9054e878742ce71478b71
Edinaldo Nogueira <edinaldomoreiranog@gmail.com>
2021-10-29 11:08:49 -0300

    initial

---
+++ src/main/java/br/com/devnaldo/api_heros/ApiHerosApplication.java
@@ -0,0 +1,13 @@
+package br.com.devnaldo.api_heros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHerosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devnaldo/api_heros/ApiHerosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devnaldo.api_heros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
